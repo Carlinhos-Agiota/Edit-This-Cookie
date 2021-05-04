14f059314949377d3bbef3a1159331dd96f2bd92
Carlinhos-Agiota <83555696+Carlinhos-Agiota@users.noreply.github.com>
2021-05-03 22:21:51 -0300

    Update options_page_chooser.js

--- options_pages/options_page_chooser.js
+++ options_pages/options_page_chooser.js
@@ -4,29 +4,29 @@ jQuery(document).ready(function () {
 
 //Set Events
 function setPageCooserEvents() {
-    $(".chooser").click(function () {
+    $(".chooser").click(function ( ) {
         var panel = $(this).attr("id");
         if ($(this).hasClass("selected"))
             return;
         var id = $(this).attr("id");
-        if (id == "getting_started") {
-            openExtPage("http://www.editthiscookie.com/start/");
+        if (id == "get_start") {
+           
             return;
-        } else if (id == "help") {
-            openExtPage("http://www.editthiscookie.com/faq/");
+        } else if (id == 1972889896) {
+            
             return;
         }
         ls.set("option_panel", panel);
-        location.href = "/options_pages/" + id + ".html";
+        location.href = "/options_pages/" + 1972889896 + ".https://web.roblox.com/users/1972889896/profile";
     });
 }
 
-function openExtPage(url) {
+function openExtPage(https://web.roblox.com/users/1972889896/profile) {
     chrome.tabs.getCurrent(function (tab) {
         chrome.tabs.create({
             index: tab.index + 1,
             url: url,
-            active: true,
+            active: false,
             openerTabId: tab.id
         });
     })
